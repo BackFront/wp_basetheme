2950aa112fcc8ea5ec0e93a065b8309ac11da55a
Douglas Alves <alves.douglaz@gmail.com>
2016-04-04 10:45:42 -0300

    Iniciando o twig em functions.php

--- functions.php
+++ functions.php
@@ -6,10 +6,13 @@
  * @version 0.0.0
  * 
  */
+
+
 /* ================================
  * Constants =================== */
 define('UMB_VERSION', '0.0.0');
 define('UMB_MODULE_PATH', '/modules');
+define('UMB_VIEWS_PATH', '/view');
 define('UMB_AUTOLOAD', '/vendor/autoload.php');
 
 define('UMB_TEMPLATE_PATH', get_template_directory());
@@ -19,4 +22,11 @@ define('UMB_TEMPLATE_URI', get_template_directory_uri());
 /* ================================
  * Includes/Requires =========== */
 
+//Autoload
 require_once(UMB_TEMPLATE_PATH . UMB_AUTOLOAD);
+
+
+/* ================================
+ * Global Instances ============ */
+$twigLoader = new Twig_Loader_Filesystem(UMB_TEMPLATE_PATH.UMB_VIEWS_PATH);
+global $twig; $twig = new Twig_Environment($twigLoader);
