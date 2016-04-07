b95295b5c5da1ad7f1741b9cc3f9119b69d47a3d
Douglas Alves <alves.douglaz@gmail.com>
2016-04-07 16:26:33 -0300

    Custom title

--- functions.php
+++ functions.php
@@ -41,3 +41,60 @@ if (!function_exists('umb_register_styles')):
     add_action('wp_enqueue_scripts', 'umb_register_styles');
 
 endif;
+
+/* ================================
+ * Theme Settings ============== */
+if (!function_exists('umbella_theme_setup')) :
+
+    function umbella_theme_setup() {
+
+        //Theme Supports
+        add_theme_support('post-thumbnails');
+        add_theme_support('post-formats', array('aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery'));
+        add_theme_support('custom-background', apply_filters('twentyfourteen_custom_background_args', array('default-color' => 'f5f5f5')));
+
+        //Images Size
+        add_image_size('twentyfourteen-full-width', 1038, 576, true);
+
+        //Thumbnail post size
+        ////Reister the size to post thumbnail
+        set_post_thumbnail_size(1024, 768, true);
+
+        //Nav menus
+        ////Register the menus to all areas of to site
+        #register_nav_menus();
+    }
+
+    add_action('after_setup_theme', 'umbella_theme_setup');
+endif; //Theme setup
+
+/* =======================================
+ * Custom Site Title ================== */
+if (!function_exists('umb_custom_title')) :
+
+    function umb_custom_title($title, $sep) {
+        global $paged, $page;
+
+        if (is_feed()) {
+            return $title;
+        }
+
+        // Add the site name.
+        $title .= get_bloginfo('name', 'display');
+
+        // Add the site description for the home/front page.
+        $site_description = get_bloginfo('description', 'display');
+        if ($site_description && ( is_home() || is_front_page() )) {
+            $title = "$title $sep $site_description";
+        }
+
+        // Add a page number if necessary.
+        if (( $paged >= 2 || $page >= 2 ) && !is_404()) {
+            $title = "$title $sep " . sprintf(__('Page %s', 'twentyfourteen'), max($paged, $page));
+        }
+
+        return $title;
+    }
+
+    add_filter('wp_title', 'twentyfourteen_wp_title', 10, 2);
+endif;
--- header.php
+++ header.php
@@ -3,13 +3,10 @@ global $twig;
 echo $twig->render('header.phtml', array(
     "body_class" => "",
     "charset" => get_bloginfo( 'charset' ),
-    "title" => wp_title( '|', true, 'right' ),
+    "title" => wp_title( '|', false, 'right' ),
     "language_attributes" => get_language_attributes(),
     "styles" => array(
         UMB_TEMPLATE_URI . '/assets/css/bootstrap.css',
         UMB_TEMPLATE_URI . '/assets/css/mainstyle.css'
     )
-));
-return $twig->render('header.phtml', array(
-    "wp_head" => wp_head()
 ));
