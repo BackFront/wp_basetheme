d036df7ec470f2cf707ea46c378eaaa407ae3950
Douglas Alves <alves.douglaz@gmail.com>
2016-04-11 11:03:06 -0300

    the_post() na page-jomborton.php

--- page-jombotron.php
+++ page-jombotron.php
@@ -8,11 +8,12 @@
  */
 
 get_header();
+the_post();
 get_template_part('nav','main');
 
 echo $twig->render('page-jombotron.twig',array(
     'the_title' => get_the_title(),
-    'the_content' => get_the_content()
+    'the_content' => get_the_content(),
 ));
 
 get_footer();
--- page.php
+++ page.php
@@ -6,7 +6,6 @@ get_template_part('nav','main');
 echo $twig->render('page.twig',array(
     'the_title' => get_the_title(),
     'the_content' => get_the_content(),
-    'debug' => "teste"
 ));
 
 get_footer();
